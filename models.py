45a55e5cb149b841dedab0699d41842a4217aad0
prikl11 <119262114+prikl11@users.noreply.github.com>
2025-08-22 21:40:19 +0300

    Added Reminder class

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from database import Base
-from sqlalchemy import Integer, Text, String, Column, DateTime, ForeignKey
+from sqlalchemy import Integer, Text, String, Column, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 
 class User(Base):
@@ -11,6 +11,7 @@ class User(Base):
     password = Column(Text, index=True)
 
     events = relationship("Event", back_populates="user")
+    reminders = relationship("Reminder", back_populates="user")
 
 class Event(Base):
     __tablename__ = "events"
@@ -19,6 +20,18 @@ class Event(Base):
     title = Column(String(30), index=True)
     start_time = Column(DateTime, index=True)
     end_time = Column(DateTime, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
 
     user = relationship("User", back_populates="events")
+
+class Reminder(Base):
+    __tablename__ = "reminders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
+    telegram_id = Column(Integer, index=True)
+    text = Column(String(200), index=True)
+    remind_at = Column(DateTime, index=True)
+    done = Column(Boolean, index=True, default=False)
+
+    user = relationship("User", back_populates="reminders")
