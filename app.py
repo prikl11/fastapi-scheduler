80d44152d2bb9b8ba2953548798f56724d9fa357
prikl11 <119262114+prikl11@users.noreply.github.com>
2025-08-22 21:40:40 +0300

    Added reminder functionality

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+from datetime import datetime
 from fastapi import FastAPI, Depends
 from typing import Annotated
 from database import get_db, Base, engine
 from sqlalchemy.orm import Session
 from schemas import UserOut, EventOut, UserCreate, EventCreate, EventUpdate
 from crud import create_user, read_users, read_events, read_event, read_user, delete_user, delete_event, update_event, create_event
+from models import Event
+from export import generate_ics
+from fastapi.responses import StreamingResponse
+from bot import create_reminder, send_reminders
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = FastAPI()
+
+scheduler = BackgroundScheduler()
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,6 +22,16 @@ SessionDep = Annotated[Session, Depends(get_db)]
 @app.get("/")
 def read_root():
     return {"message:" "FastAPI is working!"}
+
+@app.on_event("startup")
+def start_scheduler():
+    print("Scheduler started")
+    scheduler.add_job(send_reminders, "interval", seconds=30)
+    scheduler.start()
+
+@app.on_event("shutdown")
+def shutdown_scheduler():
+    scheduler.shutdown()
 
 @app.post("/users/", response_model=UserOut)
 def create_user_route(db: SessionDep, user: UserCreate):
@@ -56,8 +74,12 @@ def export_events(db: SessionDep, user_id: int):
     events = db.query(Event).filter(Event.user_id == user_id).all()
     if not events:
         return {"message": "Events not found"}
-    
+
     ics_file = generate_ics(events)
     return StreamingResponse(ics_file,
                              media_type="text/calendar",
                              headers={"Content-Disposition": "attachment; filename='events.ics'"})
+
+@app.post("/reminders/")
+def create_reminder_route(user_id: int, telegram_id: int, text: str, remind_at: datetime, db: SessionDep):
+    return create_reminder(db, user_id, telegram_id, text, remind_at)
